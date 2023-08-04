e1d61cc2fc617137d1e40bac7d34861ae5ad1abd
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-08-04 15:48:58 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp_Assignment23
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            var userMgmt = new UserManagement();
+            while (true)
+            {
+                Console.WriteLine("Enter User Name type exit to end program");
+                string userName = Console.ReadLine();
+                if (userName.ToLower() == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine("Enter Password");
+                string password = Console.ReadLine();
+                userMgmt.CreateUser(userName, password);
+            }
+            Console.WriteLine("Exiting the application");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UserManagement.cs
@@ -0,0 +1,26 @@
+﻿using MySharedLib;
+using MySharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp_Assignment23
+{
+    public class UserManagement
+    {
+        public void CreateUser(string username, string password)
+        {
+            if (DataValidator.IsPositiveNumber(username.Length))
+            {
+                Login.LogInfo($"Creating User: {username}");
+                Console.WriteLine($"Password is: {password}");
+            }
+            else
+            {
+                Login.LogError("Invalid Username");
+            }
+        }
+    }
+}
